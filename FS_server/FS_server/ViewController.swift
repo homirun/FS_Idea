31c4947a172d2c98c5ac4032b18824dde8b766d7
homirun <hatunemiku.moco@gmail.com>
2017-11-22 01:32:06 +0900

    xcode自動生成

---
+++ FS_server/FS_server/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FS_server
+//
+//  Created by homirun on 2017/11/08.
+//  Copyright © 2017年 homirun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
